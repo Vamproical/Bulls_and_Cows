a2be7e54793ab68a1dcd05c6d2bc5865f2f72aa2
Nick <mimoun136@yandex.ru>
2020-10-11 19:12:49 +1000

    Stage 6 is finished.

--- Bulls and Cows/task/src/bullscows/GenerateCode.java
+++ Bulls and Cows/task/src/bullscows/GenerateCode.java
@@ -1,19 +1,34 @@
 package bullscows;
 
+import java.util.Random;
+
 public class GenerateCode {
-    public static String generateSecreteCode(int lengthCode) {
-        if (lengthCode > 10) {
+    private static final char[] dictionary = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z'};
+    public static String generateSecreteCode(int lengthCode, int possibleSymbols) {
+        Random random = new Random();
+        if (lengthCode > 36) {
             System.out.println("Error: can't generate a secret number with a length of " +
                     lengthCode + " because there aren't enough unique digits.");
             return null;
         }
-        StringBuilder secret = new StringBuilder(String.valueOf((int) (Math.random() * 10)));
+        StringBuilder secret = new StringBuilder();
 
         while (secret.length() != lengthCode) {
-            int random = (int) (Math.random() * 10);
-            if (!secret.toString().contains(String.valueOf(random))) {
-                secret.append(random);
+            int randomInd = (random.nextInt(possibleSymbols));
+            if (!secret.toString().contains(String.valueOf(dictionary[randomInd]))) {
+                secret.append(dictionary[randomInd]);
             }
+        }
+        System.out.print("The secret is prepared: ");
+        for (int i = 0; i < lengthCode; i++) {
+            System.out.print("*");
+        }
+        if (possibleSymbols <= 10) {
+            System.out.println(" (" + dictionary[0] + "-" + dictionary[possibleSymbols - 1] + ").");
+        } else {
+            System.out.println(" (0-9, a" + "-" + dictionary[possibleSymbols - 1] + ").");
         }
         return secret.toString();
     }
--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please, enter the secret code's length:");
+        System.out.println("Input the length of the secret code:");
         int lengthCode = scanner.nextInt();
+        System.out.println("Input the number of possible symbols in the code:");
+        int possibleSymbols = scanner.nextInt();
         System.out.println("Okay, let's start a game!");
-        String code = GenerateCode.generateSecreteCode(lengthCode);
+        String code = GenerateCode.generateSecreteCode(lengthCode, possibleSymbols);
         Game game = new Game();
         game.game(code);
     }
